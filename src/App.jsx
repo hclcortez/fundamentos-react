7e78a51578f60ab91dde825a340e04b64a23139f
Hover Cortez Leite <hclcortez@gmail.com>
2022-10-17 11:31:20 -0300

    First commit and start app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from "./Post"
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author="Hover" 
+        content=" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto ad in dolorem saepe? Ut et nobis pariatur id voluptatibus. Consectetur, iste ipsum maxime deserunt explicabo earum ab eum dignissimos non."
+      />
+
+<Post 
+        author="Joana" 
+        content=" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto ad in dolorem saepe? Ut et nobis pariatur id voluptatibus. Consectetur, iste ipsum maxime deserunt explicabo earum ab eum dignissimos non."
+      />
+    </div>
+  )
+}
+
