459241eed42086f46f3d144dd909fe173e253240
Hover Cortez Leite <hclcortez@gmail.com>
2022-10-17 13:42:47 -0300

    complement sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -8,11 +9,16 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/hclcortez.png" />
                 <strong>Hover Cortez Leite</strong>
                 <span>Web Developer </span>
 
                 <footer>
-                    <a href="">Editar Seu perfil</a>
+                    
+                    <a href="">
+                        <PencilLine size={20}/>
+                        Editar Seu perfil
+                        </a>
                 </footer>
             </div>
         </aside>
