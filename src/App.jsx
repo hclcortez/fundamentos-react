c95f303ab7a9c5c1422adfc60982847f997b63b9
Hover Cortez Leite <hclcortez@gmail.com>
2022-10-17 12:53:39 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Header } from "./components/Header/Header"
 import { Post } from "./Post"
+
+import './App.css';
 
 export function App() {
 
   return (
     <div>
+
+      <Header />
+
       <Post 
         author="Hover" 
         content=" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto ad in dolorem saepe? Ut et nobis pariatur id voluptatibus. Consectetur, iste ipsum maxime deserunt explicabo earum ab eum dignissimos non."
