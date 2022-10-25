941b50d315c7dfbd3e83f5df4659e776897eefd8
Hover Cortez Leite <hclcortez@gmail.com>
2022-10-25 09:38:38 -0300

    component avatar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar/Avatar'
 import { Comment } from '../Comment/Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/hclcortez.png" />
+                    <Avatar src="https://github.com/hclcortez.png" />
                     <div className={styles.authorInfo}>
                         <strong>Hover Cortez</strong>
                         <span>Web Developer</span>
